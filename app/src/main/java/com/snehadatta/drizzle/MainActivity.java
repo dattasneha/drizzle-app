3ac1bfd61d1fda6b102e67570524217ff5543ea1
Sneha Datta <snehadatta431@gmail.com>
2025-02-15 22:23:38 +0530

    Integrate api in views

--- app/src/main/java/com/snehadatta/drizzle/MainActivity.java
+++ app/src/main/java/com/snehadatta/drizzle/MainActivity.java
@@ -1,12 +1,18 @@
 package com.snehadatta.drizzle;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.navigation.NavigationBarView;
+import com.snehadatta.drizzle.databinding.ActivityMainBinding;
+import com.snehadatta.drizzle.presentation.SettingsFragment;
+import com.snehadatta.drizzle.presentation.current_weather.CurrentWeatherFragment;
+import com.snehadatta.drizzle.presentation.future_weather.FutureWeatherFragment;
 
 import java.util.Objects;
 
@@ -14,11 +20,50 @@ import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private CurrentWeatherFragment homeFragment;
+    private FutureWeatherFragment futureWeatherFragment;
+    private SettingsFragment settingsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         Objects.requireNonNull(getSupportActionBar()).hide();
+
+        homeFragment = new CurrentWeatherFragment();
+        futureWeatherFragment = new FutureWeatherFragment();
+        settingsFragment = new SettingsFragment();
+        loadFragment(homeFragment);
+
+        binding.btmNavBar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull android.view.MenuItem item) {
+                int menuItemId = item.getItemId();
+                if(menuItemId == R.id.home) {
+                    loadFragment(homeFragment);
+                    return true;
+                }
+                else if(menuItemId == R.id.future_weather) {
+                    loadFragment(futureWeatherFragment);
+                    return true;
+                }
+                else if (menuItemId == R.id.settings) {
+                    loadFragment(settingsFragment);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+    }
+    private void loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container, fragment)
+                    .commit();
+        }
     }
 }
--- app/src/main/java/com/snehadatta/drizzle/presentation/current_weather/CurrentWeatherFragment.java
+++ app/src/main/java/com/snehadatta/drizzle/presentation/current_weather/CurrentWeatherFragment.java
@@ -2,38 +2,84 @@ package com.snehadatta.drizzle.presentation.current_weather;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.snehadatta.drizzle.R;
+import com.snehadatta.drizzle.data.model.ForecastResponse;
+import com.snehadatta.drizzle.data.model.Hour;
 import com.snehadatta.drizzle.databinding.FragmentCurrentWeatherBinding;
-import com.snehadatta.drizzle.presentation.model.HourlyWeather;
+import com.snehadatta.drizzle.presentation.MainViewModel;
 import com.snehadatta.drizzle.presentation.current_weather.adapter.CurrentWeatherHourlyUpdateRecycleViewAdapter;
+import com.snehadatta.drizzle.presentation.model.HourlyWeather;
+import com.snehadatta.drizzle.util.Resource;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
 
 
 public class CurrentWeatherFragment extends Fragment {
     private FragmentCurrentWeatherBinding binding;
-
+    private  String  API_KEY = "fba9573acc654d7a995110405240808";
+    private int days = 2;
+    private String aqi = "yes";
+    private String alerts = "yes";
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentCurrentWeatherBinding.inflate(inflater,container,false);
         View view = binding.getRoot();
 
-        List<HourlyWeather> hourlyWeatherList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            hourlyWeatherList.add(new HourlyWeather("12 PM", "19°", R.drawable.ic_day_113));
+        if (getActivity() instanceof AppCompatActivity) {
+            Objects.requireNonNull(((AppCompatActivity) getActivity()).getSupportActionBar()).hide();
         }
 
-        binding.hourlyUpdateRecycleView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
-        binding.hourlyUpdateRecycleView.setAdapter(new CurrentWeatherHourlyUpdateRecycleViewAdapter(hourlyWeatherList));
+        MainViewModel viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+
+        viewModel.getForeCast(API_KEY,"Kolkata", days,aqi,alerts);
+
+        viewModel.getForecastLiveData().observe(getViewLifecycleOwner(), resource -> {
+            if (resource instanceof Resource.Loading) {
+                binding.progressBar.setVisibility(View.VISIBLE);
+            } else if (resource instanceof Resource.Success) {
+                binding.progressBar.setVisibility(View.GONE);
+                ForecastResponse data = resource.getData();
+
+                binding.locationTextView.setText(data.getLocation().toString());
+                binding.weatherTextView.setText(data.getCurrent().getCondition().getCode());
+                binding.feelsLikeTextView.setText(
+                        String.format(Locale.US, "Feels Like: %.1f°", data.getCurrent().getFeelslikeC())
+                );
+
+                List<HourlyWeather> hourlyWeatherList = new ArrayList<>();
+                int noOfHours = data.getForecast().getForecastday().get(0).getHour().size();
+                for (int i = 0; i < noOfHours; i++) {
+                    Hour hour = data.getForecast().getForecastday().get(0).getHour().get(i);
+                    String temp = String.valueOf(hour.getTempC());
+                    String time = hour.getTime();
+                    hourlyWeatherList.add(new HourlyWeather(time, temp+"°", R.drawable.ic_day_113));
+                }
+                binding.hourlyUpdateRecycleView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+                binding.hourlyUpdateRecycleView.setAdapter(new CurrentWeatherHourlyUpdateRecycleViewAdapter(hourlyWeatherList));
+
+
+            } else if (resource instanceof Resource.Error) {
+                binding.progressBar.setVisibility(View.GONE);
+                String errorMessage = resource.getMessage();
+
+                Toast.makeText(getContext(), errorMessage, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return view;
     }
@@ -42,5 +88,8 @@ public class CurrentWeatherFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+        if (getActivity() instanceof AppCompatActivity) {
+            Objects.requireNonNull(((AppCompatActivity) getActivity()).getSupportActionBar()).show();
+        }
     }
 }
