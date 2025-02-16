6211469478ae9b88032c6e380ee4f3706fccf399
Sneha Datta <snehadatta431@gmail.com>
2025-02-16 09:14:51 +0530

    Add preferences in SettingsFragment

--- app/src/main/java/com/snehadatta/drizzle/presentation/SettingsFragment.java
+++ app/src/main/java/com/snehadatta/drizzle/presentation/SettingsFragment.java
@@ -1,8 +1,13 @@
 package com.snehadatta.drizzle.presentation;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.preference.EditTextPreference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreference;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,57 +15,75 @@ import android.view.ViewGroup;
 
 import com.snehadatta.drizzle.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class SettingsFragment extends Fragment {
+import java.util.Objects;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private static final String KEY_USE_DEVICE_LOCATION = "USE_Device_Location";
+    private static final String KEY_CUSTOM_LOCATION = "CUSTOM_LOCATION";
     public SettingsFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
-        SettingsFragment fragment = new SettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
+    @NonNull
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        return inflater.inflate(R.layout.fragment_future_weather, container, false);
+    }
+
+
+    @Override
+    public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+        updateSummary();
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Objects.requireNonNull(getPreferenceScreen().getSharedPreferences()).registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Objects.requireNonNull(getPreferenceScreen().getSharedPreferences()).unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, @Nullable String key) {
+        assert key != null;
+        if (key.equals(KEY_USE_DEVICE_LOCATION) || key.equals(KEY_CUSTOM_LOCATION)) {
+            updateSummary();
+        }
+    }
+    private void updateSummary() {
+        SharedPreferences sharedPreferences = getPreferenceManager().getSharedPreferences();
+
+        SwitchPreference useDeviceLocationPref = findPreference(KEY_USE_DEVICE_LOCATION);
+        EditTextPreference customLocationPref = findPreference(KEY_CUSTOM_LOCATION);
+
+        assert sharedPreferences != null;
+        boolean useDeviceLocation = sharedPreferences.getBoolean(KEY_USE_DEVICE_LOCATION, true);
+        String customLocation = sharedPreferences.getString(KEY_CUSTOM_LOCATION, "Kolkata");
+
+        if (useDeviceLocation) {
+            if (customLocationPref != null) {
+                customLocationPref.setEnabled(false);
+                customLocationPref.setSummary("Using Device Location");
+            }
+        } else {
+            if (customLocationPref != null) {
+                customLocationPref.setEnabled(true);
+                customLocationPref.setSummary("Current: " + customLocation);
+            }
+        }
     }
 }
