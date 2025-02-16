a751c9c50722cc9f974eb708b3fa6d23524b0461
Sneha Datta <snehadatta431@gmail.com>
2025-02-16 10:20:20 +0530

    Fix minor issues

--- app/src/main/java/com/snehadatta/drizzle/presentation/SettingsFragment.java
+++ app/src/main/java/com/snehadatta/drizzle/presentation/SettingsFragment.java
@@ -32,9 +32,8 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
 
     @NonNull
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_future_weather, container, false);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 
 
